a8a66ed60ca4b63a2458a8a5a1f6f2be55396ada
zhujie <zhujie_12@126.com>
2019-04-15 22:29:10 +0800

    空调调度系统

---
+++ src/test/java/com/pigteam/airconditioning/dspatchparent/DspatchParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pigteam.airconditioning.dspatchparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DspatchParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
